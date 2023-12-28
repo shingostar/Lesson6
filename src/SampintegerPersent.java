2711642d0738c3224b4d9a1d307e4bfed15818f5
shingostar <shingostar@shore.ocn.ne.jp>
2023-12-28 22:59:11 +0900

    Lesson6のサンプルをコミット。

---
+++ src/SampintegerPersent.java
@@ -0,0 +1,11 @@
+
+public class SampintegerPersent {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ@9o
+        int i = 123;
+        String s = "456";
+        System.out.println(i + Integer.parseInt(s));
+    }
+
+}
---
+++ src/length.java
@@ -0,0 +1,10 @@
+
+public class length {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        String s = "abc";
+        System.out.println(s.length());
+    }
+
+}
